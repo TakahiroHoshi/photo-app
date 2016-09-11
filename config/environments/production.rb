d2d36c34b624069238a5bc9212fa8a723526c512
= <loinzaft@gmail.com>
2016-09-11 14:19:20 +0900

    edited production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'https://tk-photo-app.herokuapp.com', protocol: 'https' }
+  config.action_mailer.default_url_options = { :host => 'https://tk-photo-app.herokuapp.com', :protocol =>'https' }
 
 end
